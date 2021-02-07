55257a05e03e46775e2d114f32b09d6946e6ad81
mahmoud-ow <52075236+mahmoud-ow@users.noreply.github.com>
2021-02-07 03:45:39 -0800

    vid 11 start

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create New Project</title>
+</head>
+
+<body>
+
+    <h1>Create Project</h1>
+
+    <form action="/projects" method="POST">
+        @csrf
+        <div>
+            <input type="text" name="title" placeholder="Project Title">
+        </div>
+
+        <div>
+            <textarea name="description" placeholder="Project Description"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Create Project</button>
+        </div>
+
+    </form>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::post('/projects', [ProjectController::class, 'store']);
+Route::get('/projects/create', [ProjectController::class, 'create']);
